dd46fdbdc782a43ea97fa29c26daa973d7a6c703
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-30 00:30:19 +0000

    added initialize, store_birthday, see and check_birthday methods, and their corresponding Unit Tests

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,29 @@
+require 'date'
 
+class BirthdayList
+
+  attr_reader :birthdays
+
+  def initialize
+    @birthdays = []
+  end
+
+  def store_birthday(name, date)
+    @birthdays << [name, date]
+  end
+
+  def see
+    return @birthdays.map {|item| p item.join(" - ")}
+  end
+
+  def check_birthday(date)
+    @birthdays.each { |item|
+      if item[1] == date
+        age = (Date.today.year - (item[1][/[0-9]+$/]).to_i) % 100
+        p "It's #{item[0]}'s birthday today! They are #{age} years old!"
+        return item[0]
+      end
+    }
+  end
+
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,49 @@
+require 'birthday_list'
 
+describe BirthdayList do
+
+  it "responds to birthdays" do
+    expect(subject).to respond_to :birthdays
+  end
+
+  describe "store_birthday" do
+    it "responds to store_birthday" do
+      expect(subject).to respond_to :store_birthday
+    end
+
+    it "accepts two arguments from the user" do
+      expect(subject).to respond_to(:store_birthday).with(2).arguments
+    end
+
+    it "stores name and birthday in birthday list" do
+      subject.store_birthday("Constantine", "29/01/95")
+      expect(["Constantine", "29/01/95"]).to eq subject.birthdays[-1]
+    end
+  end
+
+  describe "see" do
+    it "responds to see" do
+      expect(subject).to respond_to :see
+    end
+
+    it "prints out the name - birthday pairs on separate lines" do
+      subject.store_birthday("Constantine", "29/01/95")
+      expect(subject.see).to eq(["Constantine - 29/01/95"])
+    end
+  end
+
+  describe "check_birthday" do
+    it "responds to check_birthday" do
+      expect(subject).to respond_to(:check_birthday)
+    end
+
+    it "accepts an argument" do
+      expect(subject).to respond_to(:check_birthday).with(1).argument
+    end
+
+    it "matches a birthday on the birthday list" do
+      subject.store_birthday("Constantine", "29/01/95")
+      expect(subject.check_birthday("29/01/95")).to eq "Constantine"
+    end
+  end
+end
